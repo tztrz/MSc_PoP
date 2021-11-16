62e9c3e114ee50f6051c11e3da383e0d6baded56
LOUISA PRICE <louisaprice0@gmail.com>
2021-11-16 13:32:02 +0000

    switching to pycharm

--- chess_puzzle.py
+++ chess_puzzle.py
@@ -1,8 +1,7 @@
 def location2index(loc: str) -> tuple[int, int]:
     '''converts chess location to corresponding x and y coordinates'''
-    location = [ord(x) - 96 for x in loc]
-    index = tuple(location)
-    return index
+    location = [loc.split()]
+    
 	
 def index2location(x: int, y: int) -> str:
     '''converts  pair of coordinates to corresponding location'''
